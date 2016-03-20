6a0f95ffbb6a13d87f8650ee3f4b1f22f4903986
jgarcia77 <jgarcia773@gmail.com>
2016-03-20 15:14:50 -0500

    Update image scaling

---
+++ Common.Helpers.Tests/DrawingTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common.Helpers.Drawing;
+
+namespace Common.Helpers.Tests
+{
+    [TestClass]
+    public class DrawingTests
+    {
+        [TestMethod]
+        public void NoScaleTest()
+        {
+            var quadrilateral = new Quadrilateral(500, 500);
+            var scale = quadrilateral.Scale(500, 500);
+
+            Assert.AreEqual(quadrilateral.Width, scale.Width);
+            Assert.AreEqual(quadrilateral.Height, scale.Height);
+        }
+
+        [TestMethod]
+        public void ScaleDownUsingWidthTest()
+        {
+            var quadrilateral = new Quadrilateral(500, 500);
+            var scale = quadrilateral.Scale(851, 314);
+
+            Assert.AreEqual(500, scale.Width);
+            Assert.AreEqual(184, scale.Height);
+        }
+
+        [TestMethod]
+        public void ScaleDownUsingHeightTest()
+        {
+            var quadrilateral = new Quadrilateral(500, 500);
+            var scale = quadrilateral.Scale(314, 851);
+
+            Assert.AreEqual(184, scale.Width);
+            Assert.AreEqual(500, scale.Height);
+        }
+
+        [TestMethod]
+        public void ScaleUpUsingWidthTest()
+        {
+            var quadrilateral = new Quadrilateral(500, 500);
+            var scale = quadrilateral.Scale(100, 50);
+
+            Assert.AreEqual(500, scale.Width);
+            Assert.AreEqual(250, scale.Height);
+        }
+
+        [TestMethod]
+        public void ScaleUpUsingHeightTest()
+        {
+            var quadrilateral = new Quadrilateral(500, 500);
+            var scale = quadrilateral.Scale(50, 100);
+
+            Assert.AreEqual(250, scale.Width);
+            Assert.AreEqual(500, scale.Height);
+        }
+
+        [TestMethod]
+        public void ScaleDownMultipleTest()
+        {
+            var quadrilateral = new Quadrilateral(500, 500);
+            var scale = quadrilateral.Scale(851, 314);
+
+            Assert.AreEqual(500, scale.Width);
+            Assert.AreEqual(184, scale.Height);
+
+            var quadrilateral2 = new Quadrilateral(300, 300);
+            var scale2 = quadrilateral2.Scale(scale.Width, scale.Height);
+
+            Assert.AreEqual(300, scale2.Width);
+            Assert.AreEqual(110, scale2.Height);
+        }
+    }
+}
